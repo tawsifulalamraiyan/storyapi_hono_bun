9fca9c128fa1dc045fa1bfded30c374e3004b4a2
tawsifulalamraiyan <tawsifulalam2002@gmail.com>
2025-10-16 12:05:05 +0600

    first commit

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,6 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-const welcomeStrings = [
-  "Hello Hono!",
-  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
-];
-
-app.get("/", (c) => {
-  return c.text(welcomeStrings.join("\n\n"));
-});
+app.get("/", (c) => c.text("Hono!"));
 
 export default app;
