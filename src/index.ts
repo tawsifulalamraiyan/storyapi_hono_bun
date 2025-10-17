f45624fc0b3d1d84db96d4f1494bbe009464d7cb
tawsifulalamraiyan <tawsifulalam2002@gmail.com>
2025-10-17 07:25:12 +0600

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { prisma } from "./lib/prisma.js";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(cors());
 
 app.get("/", (c) => {
   return c.json("hello");
